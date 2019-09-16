a666196506ba2518baf2e072c48e80de6c5b18b9
cruzayye <johnnyc010392@gmail.com>
2019-09-16 15:13:38 -0700

    styles coming in from css file and inline styling

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import Styles from './Slider.css'
 
+const bgstyle = {
+  background: 'red',
+}
+
 const Slider = () => {
   return (
-    <section className={Styles.background}>
+    <section className={Styles.background} style={bgstyle}>
       <h2>Slider goes here</h2>
     </section>
   );
